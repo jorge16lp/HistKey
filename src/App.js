de29495164904821333ce2c33b687235d7413213
Jorge <jor.lp.16@gmail.com>
2023-06-27 15:56:21 +0200

    URL change again

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { FaEdit, FaTrash } from 'react-icons/fa'
 import { TiTick } from 'react-icons/ti'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
-// const endPoint = process.env.REACT_APP_API_URI || 'http://localhost:3001'
-const endPoint = process.env.REACT_APP_API_URI || 'https://jorge16lp.github.io/Histkey/'
+const endPoint = process.env.REACT_APP_API_URI || 'http://localhost:3001'
+// const endPoint = process.env.REACT_APP_API_URI || 'https://jorge16lp.github.io/Histkey/'
 var text = ''
 
 export default function App() {
